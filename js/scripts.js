3b0eea848ef3feb16624e4e939b2396cac7eb779
“Andrew” <wjboff@hotmail.co.uk>
2026-03-13 08:37:47 +0000

    finished meets

--- js/scripts.js
+++ js/scripts.js
@@ -56,10 +56,37 @@ async function fetchUsers() {
 // create modal container, add 'modal-container' class and add it to the page (after #gallery)
 let modalContainer = document.createElement('div');
 modalContainer.classList.add('modal-container'); 
+
+// function to display the modal, passing in the person we want to do this for
+function displayModal(person) {
+    // format their birthday by parsing their date property, and formatting to US date format
+    let birthday = new Date(Date.parse(person.dob.date));
+    let dateFormat = new Intl.DateTimeFormat("en-US");
+    let newBirthday = dateFormat.format(birthday);
+    // add relevant person info to modalContainer in desired HTML foramt
+    modalContainer.innerHTML = `
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.phone}</p>
+                <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.postcode}</p>
+                <p class="modal-text">Birthday: ${newBirthday}</p>
+            </div>
+        </div>        
+        `;
+    // display the container
+    modalContainer.style.display = 'flex';
+}
+
 // set it's display to initially none
 modalContainer.style.display = 'none';
 gallery.insertAdjacentElement('afterend', modalContainer);
-// listen for clicks on parent element of person cards so we listen for clicks on people using bubbling
+// listen for clicks on parent element of person cards for clicks on people using bubbling
 
 gallery.addEventListener('click', (e) => {
     // check that it was a person card that was clicked
@@ -68,38 +95,16 @@ gallery.addEventListener('click', (e) => {
     if (e.target.classList.contains('card')) {
         // get the person via matching person card id with people array index
         const person = people[e.target.id];
-        console.log(e);
-        console.log(person);
-        // format their birthday by parsing their date property, and formatting to US date format
-        let birthday = new Date(Date.parse(person.dob.date));
-        let dateFormat = new Intl.DateTimeFormat("en-US");
-        let newBirthday = dateFormat.format(birthday);
-        // use this name to search in people array for the person
-        // add relevant person info to modalContainer
-        modalContainer.innerHTML = `
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-                    <p class="modal-text">${person.email}</p>
-                    <p class="modal-text cap">${person.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${person.phone}</p>
-                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${newBirthday}</p>
-                </div>
-            </div>        
-            `;
-        // display the container
-        modalContainer.style.display = 'flex';
+        // pass in this person to the displayModal function
+        displayModal(person);
+    // repeat process for elements inside the 'card; parent
     } else if (e.target.parentElement.classList.contains('card')) {
         const person = people[e.target.parentElement.id];
-        console.log(person);
+        displayModal(person);
 
     } else if (e.target.parentElement.parentElement.classList.contains('card')) {
         const person = people[e.target.parentElement.parentElement.id];
-        console.log(person);
+        displayModal(person);
     } 
 });
 
@@ -107,11 +112,22 @@ gallery.addEventListener('click', (e) => {
 // ... as that might trigger the click, in which case we want it's parent node 
 // If it was, hide the modal container
 modalContainer.addEventListener('click', (e) => {
-    console.log(e);
     if (e.target.id === 'modal-close-btn' || e.target.parentElement.id === 'modal-close-btn') {
+        modalContainer.style.display = 'none';
+    // but if the modal container was clicked, we want to close the modal (since this means...
+    // ... that outside the modal itself was clicked
+    } else if (e.target.className === "modal-container") {
         modalContainer.style.display = 'none';
     }
 })
+
+// listen for escape key being pressed, and close the modal
+document.body.addEventListener('keyup', (e) => {
+    // if the target was the escape key
+    if (e.key === 'Escape') {
+        modalContainer.style.display = 'none';
+    }
+});
 // call fetchUsers to run the program
 fetchUsers();
 
