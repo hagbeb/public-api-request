0656204f2e78c27d5ac6799a0a3d438b16512241
“Andrew” <wjboff@hotmail.co.uk>
2026-03-22 15:34:21 +0000

    slider solution 2

--- js/scripts.js
+++ js/scripts.js
@@ -170,10 +170,13 @@ function getModalPosition(element, position) {
     // get the coords of the modal element
     let rect = element.getBoundingClientRect();
     let newPosition = 0;
+    // if we asked for the 'left' position, return left position, else return the right position
     if (position === 'left') {
         newPosition = rect.left + document.documentElement.scrollLeft;
     } else if (position === 'right') {
-        newPosition = rect.right + document.documentElement.scrollLeft;
+        // new right position is the page width minus the 'right' coordinate (since getBoundingClientRect returns coordinates...
+        // ... relative to the left of the page, but to set 'right' position we want the number from the right of the page)
+        newPosition = Number(document.body.offsetWidth) - Number(rect.right + document.documentElement.scrollLeft);   
     }
     return newPosition;
 };
@@ -195,8 +198,6 @@ buttonsContainer.addEventListener('click', (e) => {
         }
     // else if the previous button was clicked
     } else if (e.target.textContent === 'Prev') {
-        console.log('current person after previous click: ', people[index]);
-        console.log('index: ', index);
         // if the index number is greater than 0 (since we dont' want to subtract/go back from 0)
         if (index > Number(0)) {
             // subtract 1 from index representing person to show, then use index to pass that person into displayModal.
@@ -227,33 +228,30 @@ function generateModalHTML(person) {
 
 // function which transitions the modal we pass into it into the central, visible modal position
 function transitionModal(element) {
-    // display modal by removing hide-class, + add classes for it's new design
+    // display modal by removing hide-class, + add classes to reflect 'currentModal' design
     element.classList.remove('hide-modal');
     element.classList.add('modal-info-container', 'extra-modals');
     // Force reflow to ensure transition works - THANKS TO TRAVIS ALSTRAND
     element.offsetWidth;
-    nextModal.offsetWidth;
     // add the class to slide in the modal
     if (element === previousModal) {
         element.classList.add('slide-left');
     }   else if (element === nextModal) {
         element.classList.add('slide-right');
     }
+        console.log('transitionModal finished');
+
 }
 
 // function to replace the previously 'current' modal with the one we transitioned into its place
 // we pass in the modal we transitioned, + the class specific to it that we need to remove
-function replaceModal(element, replaceClass) {
+function replaceModal(element, replaceClass1, replaceClass2) {
     // remove the classes associated with non-current modals
-    if (element === previousModal) {
-        element.classList.remove('slide-left', replaceClass, 'extra-modals');
-    }   else if (element === nextModal) {
-        element.classList.remove('slide-right', replaceClass, 'extra-modals');
-    }
+    element.classList.remove(replaceClass1, replaceClass2, 'extra-modals');
     // make 'previous' the new current - add previousModal to modal contanier, remove 'currentModal'
     innerModal.insertBefore(element, buttonsContainer);
-    //console.log('innerModal: ', innerModal);
     currentModal.remove();
+    console.log('replaceModal finished');
 }
 
 // function to create new modal to replace the one we moved into the 'current' position
@@ -291,26 +289,17 @@ function createNewModal(element, addClass, newID) {
     // assign the 'currentModal' variable to the modal that we moved into the current position
     currentModal = document.getElementById(newID);
     // assign the variable for the modal we moved to the placeholder, ie it's replacement
-    // We have to assign to the names 'previousModal' & 'nextModal', not 'element' , NOT SURE WHY - IS IT BECAUSE FOR ASSIGNMENTS...
-    // ... WE NEED TO USE THE EXISTING REFERENCE, NOT THE COPY OF THE REFERENCE?
-        // THESE LINES DON'T WORK - WE NEED TO USE THE BELOW 'IF' CONDITIONAL INSTEAD
-            // element = document.getElementById('placeholder');
-            // element.id = newID;
-    
     if (element === previousModal) {
         previousModal = document.getElementById('placeholder');
         // change id attributes to relect the above
         previousModal.id = newID;
-            //console.log('new modal: ', nextModal);
     } else if (element === nextModal) {
         nextModal = document.getElementById('placeholder');
         // change id attributes to relect the above
         nextModal.id = newID;
-        //console.log('new modal: ', nextModal);
-    }
-        
-    //console.log('currentModal: ', currentModal);
+    }        
     currentModal.id = 'current';
+        console.log('createModal finished');
 }
 
 // function to display modal when a person is clicked, or next/previous button. Pass in the person + the button
@@ -326,7 +315,7 @@ function displayModal(person, buttonClicked) {
         // Update position of 'next'/'previous' modals using position of current modal
         // update the 'left'/'right' properties used by the 'slide' classes which do the transitions, using currentModals position
         root.style.setProperty('--modal-left-position', `${getModalPosition(currentModal, 'left')}px`);
-        root.style.setProperty('--modal-right-position', `${getModalPosition(currentModal, 'left')}px`);
+        root.style.setProperty('--modal-right-position', `${getModalPosition(currentModal, 'right')}px`);
     // Generate 'next' and 'previous' modal HTML using next/previous people in the array
         // if the index is less than the maximum index number (which is the no. of people minus one)...
         // .. then generate next modal
@@ -346,7 +335,7 @@ function displayModal(person, buttonClicked) {
         // once the new modal is in position
         setTimeout(() => {
             // replace the previously 'current' modal with the one we transitioned into its place (which we pass in) as the main modal
-            replaceModal(previousModal, 'previous-modal');
+            replaceModal(previousModal, 'slide-left', 'previous-modal');
             // create new 'previous' modal to replace the one we moved into the 'current' position
             createNewModal(previousModal, 'previous-modal', 'previous');
         }, 200);
@@ -356,10 +345,11 @@ function displayModal(person, buttonClicked) {
         // when the transition is finished & new modal is in place
         setTimeout(() => {
             // replace the previously 'current' modal with the one we transitioned into its place (which we pass in)
-            replaceModal(nextModal, 'next-modal');
+            replaceModal(nextModal, 'slide-right', 'next-modal');
             // create new modal to replace the one we moved into the 'current' position
             createNewModal(nextModal, 'next-modal', 'next');
         }, 200);
+        console.log('timeout finished');
     }
 }
 
@@ -377,7 +367,6 @@ gallery.addEventListener('click', (e) => {
         // save the index of the person in the modal
         index = e.target.id;
         // pass in this person to the displayModal function
-        //console.log('person before displayModal for gallery click 1', people[index]);
         displayModal(people[index], 'gallery');
 
 
@@ -385,7 +374,6 @@ gallery.addEventListener('click', (e) => {
     } else if (e.target.parentElement.classList.contains('card')) {
         person = people[e.target.parentElement.id];
         index = e.target.parentElement.id;
-        //console.log('person before displayModal for gallery click 3', people[index]);
         displayModal(people[index], 'gallery');
 
 
